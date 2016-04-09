0a5afb8c098f7223ad9c360e0c62ffd561172548
puhitaku <puhitaku@gmail.com>
2016-04-09 21:10:41 +0900

    Remove useless funcs

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from __future__ import print_function, unicode_literals
+
 import sqlite3
-import json
 from flask import Flask, request as req, jsonify
 from flask.ext.cors import CORS
-from manager import MapPlayerManager, GameMaster
 
 
 app = Flask(__name__)
 CORS(app)
-manager = MapPlayerManager()
-gm = GameMaster()
 _sql = sqlite3.connect('database.db', check_same_thread=False)
 sql = _sql.cursor()
 
@@ -20,45 +18,6 @@ def index():
 def admin():
     return app.send_static_file('admin.html')
 
-@app.route('/add')
-def add():
-    args = req.args
-    name, pid = args.get('name'), args.get('pid')
-    sql.execute('INSERT INTO players (name) VALUES ("%s")' % name)
-    _sql.commit()
-    if manager.add(name, pid):
-        return 'OK'
-    else:
-        return 'NG'
-
-@app.route('/list')
-def list():
-    return str(manager.get_list())
-
-@app.route('/kill')
-def kill():
-    args = req.args
-    if manager.kill(args.get('killer_pid'), args.get('killed_pid')):
-        return 'OK'
-    else:
-        return 'NG'
-
-@app.route('/resurrect')
-def resurrect():
-    args = req.args
-    if manager.resurrect(args.get('pid')):
-        return 'OK'
-    else:
-        return 'NG'
-
-@app.route('/start')
-def start():
-    gm.getready()
-    return 'Started.'
-
-@app.route('/isready')
-def isready():
-    return str(gm.is_ready())
 
 if __name__ == '__main__':
     sql.execute('DROP TABLE IF EXISTS players')
