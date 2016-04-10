0cf7ade058af085d9d5e06780218d4be0c902e9d
puhitaku <puhitaku@gmail.com>
2016-04-10 10:18:06 +0900

    db

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ SUCCESS, FAIL = 'SUCCESS', 'FAIL'
 
 @app.route('/')
 def index():
-    return jsonify({'msg': 'Hello Sonoba!', 'db_url': url})
+    return jsonify({'msg': 'Hello Sonoba!', 'db_info': url.__dict__})
 
 
 @app.route('/rooms', methods=['GET'])
